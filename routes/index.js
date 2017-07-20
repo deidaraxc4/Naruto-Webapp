013b38ba6aa7df8abb7c97d8c18706c15ce9bc85
Nick Tang <nicktang@MacBook-Pro.local>
2017-07-20 15:57:09 -0400

    added css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , ip: req.ip});
+  res.render('index', { title: 'Express' , ip: req.ip, weather: 'rain'});
 });
 
 module.exports = router;
